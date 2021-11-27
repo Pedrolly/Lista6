2ba1ec6a0e5b728de49a5e30922681ea829052e5
Pedrolly <86805506+Pedrolly@users.noreply.github.com>
2021-11-27 20:24:52 -0300

    Lista 6

---
+++ src/main/java/Pessoa.java
@@ -0,0 +1,13 @@
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private String curso;
+
+    public Pessoa(String nome, int idade, String curso) {
+        this.nome = nome;
+        this.idade = idade;
+        this.curso = curso;
+    }
+
+}
+
